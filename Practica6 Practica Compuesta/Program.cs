b697a307ced0296fa51317bcc2b153aad08b3d01
Isra <Isra@DESKTOP-64KKLPF>
2023-10-05 21:05:53 -0600

    Agregar archivos de proyecto.

---
+++ Practica6 Practica Compuesta/Program.cs
@@ -0,0 +1,20 @@
+﻿
+double num1, num2, suma, resta, mult, divis, res;
+Console.WriteLine("Ingrese un número: ");
+num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo numero: ");
+num2 = int.Parse(Console.ReadLine());
+
+suma = num1 + num2;
+resta = num1 - num2;
+mult = num1 * num2;
+divis = num1 / num2;
+
+Console.WriteLine("La suma de los números es: " + suma);
+Console.WriteLine("La resta de los números  es : " + resta);
+Console.WriteLine("La multiplicación de los números es: " + mult);
+Console.WriteLine("La división de los números es:" + divis);
+res = (suma * resta) * mult / (divis);
+Console.WriteLine("La suma x la resta x la mult dividida es: " + res);
+
+Console.ReadKey();
